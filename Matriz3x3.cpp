76a831f69294d0cd060335d52411d3654f6796c3
TeslaBv <99981494+TeslaBv@users.noreply.github.com>
2022-02-18 10:04:45 -0600

    Add files via upload

---
+++ Matriz3x3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	float f1c1,f1c2,f1c3,f1c4,f2c1,f2c2,f2c3,f2c4,f3c1,f3c2,f3c3,f3c4;
+	printf("Ingrese los valores de la matriz\n");
+	scanf("%f",&f1c1);scanf("%f",&f1c2);scanf("%f",&f1c3);
+	scanf("%f",&f2c1);scanf("%f",&f2c2);scanf("%f",&f2c3);
+	scanf("%f",&f3c1);scanf("%f",&f3c2);scanf("%f",&f3c3);
+	printf("%.2f	%.2f	%.2f	%.2f",f1c1,f1c2,f1c3);
+	
+	
+	
+	
+	return 0;
+}
+
